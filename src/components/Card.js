b89d94c8b597855e6d076d92c2fdf13b18271521
Debayan Das <debayan930@gmail.com>
2020-03-04 12:51:12 +0530

    completed robofriends

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Card.css';
 
 const Card = (props) => {
 	const { id, name, email } = props.robot;
 	return(
-		<div className='tc grow br3 pa2 ma2 dib bw2 shadow-5 bg-light-green'>
-			<img src={`https://robohash.org/${id}?size=200x200`} alt='kitty'/ >
+		<div className='card tc grow br4 pa3 ma2 dib bw2 shadow-5 bg-light-green'>
+			<img src={`https://robohash.org/${id}?set=set4&size=175x175`} alt='kitty'/ >
 			<div>
 				<h3>{name}</h3>
 				<p>{email}</p>
